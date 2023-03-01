659fd0504a1668280b9479379438182699773ed5
José Carlos <jose.carlos.araujo07@aluno.ifce.edu.br>
2023-03-01 12:48:51 -0300

    :sparkles: login page componented created

---
+++ src/pages/login-page/login-page.tsx
@@ -0,0 +1,11 @@
+import * as S from './login-page.styles';
+
+const LoginPage: React.FC = () => {
+  return (
+    <S.Container>
+      <h1>Welcome to LoginPage!</h1>
+    </S.Container>
+  );
+};
+
+export default LoginPage;
