e5b9870299378149047ef3a7bb6571533f2c8318
José Carlos <jose.carlos.araujo07@aluno.ifce.edu.br>
2023-03-01 11:59:11 -0300

    :wrench: chore: dependencys updated and files cleaned

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 
-import NxWelcome from './nx-welcome';
-
 const StyledApp = styled.div`
   // Your style here
 `;
@@ -9,7 +7,12 @@ const StyledApp = styled.div`
 export function App() {
   return (
     <StyledApp>
-      <NxWelcome title="to-do" />
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+        ducimus nam ipsam vero aperiam odio sapiente veniam, quas quidem optio
+        reprehenderit voluptate deserunt, rerum distinctio, placeat accusamus
+        officiis dolorem voluptatem.
+      </div>
     </StyledApp>
   );
 }
