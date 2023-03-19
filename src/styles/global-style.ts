7857ed287047eda1b5d3f118bd177b0b74b8fe5f
José Carlos <jose.carlos.araujo07@aluno.ifce.edu.br>
2023-03-19 17:55:54 -0300

    :oh_hand: style: global-style improved with resets

--- src/styles/global-style.ts
+++ src/styles/global-style.ts
@@ -2,11 +2,33 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     font-family: 'Poppins', sans-serif;
+  }
+
+  html, body {
+    height: 100%;
+  }
+
+  img, picture, video, canvas, svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  input, button, textarea, select {
+  font: inherit;
+}
+
+  p, h1, h2, h3, h4, h5, h6 {
+    overflow-wrap: break-word;
+  }
+
+  #root {
+    isolation: isolate;
+
   }
 `;
 
