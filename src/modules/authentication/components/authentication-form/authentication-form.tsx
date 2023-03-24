b607358b1424604134d0004699f89e3b519703b2
José Carlos <jose.carlos.araujo07@aluno.ifce.edu.br>
2023-03-24 16:32:08 -0300

    :sparkles: feat: form improved with hook form types

--- src/modules/authentication/components/authentication-form/authentication-form.tsx
+++ src/modules/authentication/components/authentication-form/authentication-form.tsx
@@ -1,19 +1,29 @@
-import { FormTitle, Logo, TextInput } from 'src/shared';
+import { SubmitHandler, useFormContext } from 'react-hook-form';
+import {
+  AuthenticationFormInput,
+  FormTitle,
+  Logo,
+  TextInput,
+} from 'src/shared';
 import Button from 'src/shared/components/button/button';
 import * as S from './authentication-form.styles';
 
 type Props = {
   isRegister?: boolean;
+  onSubmit: SubmitHandler<AuthenticationFormInput>;
 };
 
-const AuthenticationForm: React.FC<Props> = ({ isRegister }) => {
+const AuthenticationForm: React.FC<Props> = ({ isRegister, onSubmit }) => {
+  const { handleSubmit, register, formState } =
+    useFormContext<AuthenticationFormInput>();
+
   return (
     <S.ContentWrapper>
       <S.ContentContainer>
         <S.LogoWrapper>
           <Logo />
         </S.LogoWrapper>
-        <S.FormWrapper>
+        <S.FormWrapper onSubmit={handleSubmit(onSubmit)}>
           <FormTitle>{isRegister ? 'Nova Conta' : 'Autenticação'}</FormTitle>
           <S.InputContainer>
             {isRegister && (
@@ -22,6 +32,8 @@ const AuthenticationForm: React.FC<Props> = ({ isRegister }) => {
                 type="text"
                 placeholder="Digite seu nome"
                 isRequired={isRegister && true}
+                error={formState.errors.nome?.message}
+                {...register('nome')}
               />
             )}
             <TextInput
@@ -29,12 +41,16 @@ const AuthenticationForm: React.FC<Props> = ({ isRegister }) => {
               type="email"
               placeholder="exemplo@email.com"
               isRequired={isRegister && true}
+              error={formState.errors.email?.message}
+              {...register('email')}
             />
             <TextInput
               label="Senha"
               type="password"
               placeholder="Digite sua senha"
               isRequired={isRegister && true}
+              error={formState.errors.senha?.message}
+              {...register('senha')}
             />
             {isRegister && (
               <TextInput
@@ -42,6 +58,8 @@ const AuthenticationForm: React.FC<Props> = ({ isRegister }) => {
                 type="password"
                 placeholder="Confirme a senha"
                 isRequired={isRegister && true}
+                error={formState.errors.confirmarSenha?.message}
+                {...register('confirmarSenha')}
               />
             )}
           </S.InputContainer>
